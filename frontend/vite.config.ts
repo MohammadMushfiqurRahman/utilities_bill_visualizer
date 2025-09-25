25e7d25b4c89a01026e966ee020e8a920720f025
Mohammad Mushfiqur Rahman <heemel.mushfiqur@gmail.com>
2025-09-25 23:01:17 +0600

    vite plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import path from 'path';
+import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    plugins: [react()],
     server: {
       proxy: {
         '/api': {
