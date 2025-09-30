1b4bb276f9686d5bfdbff304a61727ce1e007819
Mohammad Mushfiqur Rahman <heemel.mushfiqur@gmail.com>
2025-09-30 22:39:43 +0600

    summary card

--- frontend/components/icons.tsx
+++ frontend/components/icons.tsx
@@ -273,3 +273,21 @@ export const ListIcon: React.FC<IconProps> = (props) => (
     <line x1="3" y1="18" x2="3.01" y2="18"></line>
   </svg>
 );
+
+export const AverageIcon: React.FC<IconProps> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <line x1="4" y1="12" x2="20" y2="12"></line>
+    <circle cx="12" cy="12" r="3"></circle>
+  </svg>
+);
